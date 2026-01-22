878ec8586bd9af9a581f9df3533a693bff3cc329
Jay Crisostomo <jaycris2000@gmail.com>
2026-01-22 10:36:38 -0600

    changed socket connection line

--- frontend/thevoid/src/App.js
+++ frontend/thevoid/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://thevoidbackend.onrender.com');
+const socket = io('https://thevoidproject.onrender.com');
 
 function App() {
   const [availableNames, setAvailableNames] = useState([]);
