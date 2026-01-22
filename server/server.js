7500952e31e787808c0ed2a2735f7387cefd4630
Jay Crisostomo <jaycris2000@gmail.com>
2026-01-22 10:44:20 -0600

    changed cors origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://the-void-chat.vercel.app", // Your NEW Vercel URL
+    origin: "https://thevoidproject.vercel.app", // Your NEW Vercel URL
     methods: ["GET", "POST"],
   },
 });
